f835ec718c32b47b74d9b62950ad302ad9395d02
Daniel Simão <simaodeveloper@gmail.com>
2018-07-16 01:11:24 -0300

    Fix Once method and add more Error messages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,51 +1,56 @@
-
 export default class Emmiter {
-  constructor() {
-    this.store = {};
-  }
-
-  on(event, listener) {
-    // If event Store don't exists, create it!
-    if (!this.store[event]) {
-      this.store[event] = new Set([listener]);
-    } else {
-      this.store[event].add(listener);
-    }
-  }
-
-  off(event, listener) {
-    // Verify if Event Store was created
-    if (!this.store[event]) {
-      throw new Error();
+    constructor() {
+        this.store = {};
     }
 
-    // Erase all listener from event informed
-    if (!listener) {
-      this.store[event].clear();
+    on(event, listener) {
+        // If event Store don't exists, create it!
+        if (!this.store[event]) {
+            this.store[event] = new Set([listener]);
+        } else {
+            this.store[event].add(listener);
+        }
     }
 
-    // Verify if listener exists in Event Store
-    if (!this.store[event].has(listener)) {
-      throw new Error();
+    off(event, listener) {
+        // Verify if Event Store was created
+        if (!this.store[event]) {
+            throw new Error('Please verify if this listener was created before!');
+        }
+
+        // Erase all listener from event informed
+        if (!listener) {
+            this.store[event].clear();
+        }
+
+        // Verify if listener exists in Event Store
+        if (!this.store[event].has(listener)) {
+            throw new Error('Please verify if this listener was created before!');
+        }
+
+        return this.store[event].delete(listener);
     }
 
-    return this.store[event].delete(listener);
-  }
+    once(event, listener) {
+        // Wrapper listener to be called once
+        const setListenerOnce = () => {
+            const callListener = (...args) => {
+                listener.call(null, ...args);
+                this.off(event, callListener);
+            };
 
-  once(event, listener) {
-    // Wrapper listener to be called once
-    function setListenerOnce() {
-      return (...args) => {
-        listener(null, ...args);
-        this.off(event, listener);
-      };
+            return callListener;
+        }
+
+        this.on(event, setListenerOnce());
     }
 
-    this.on(event, setListenerOnce());
-  }
+    dispatch(event, ...args) {
+        if(event in this.store) {
+            this.store[event].forEach(listener => listener.call(null, ...args));
+        } else {
+            throw new Error('Please insert a valid event!');
+        }
 
-  dispatch(event, ...args) {
-    // Dispatch all listener from Event informed
-    this.store[event].forEach(listener => listener.call(null, ...args));
-  }
+    }
 }
